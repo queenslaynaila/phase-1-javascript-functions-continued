caa1ec1a8a2eabc2727d593e46f9a7ebc96acbae
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-17 14:25:59 +0300

    assign

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(defaultActivity) {
+    if (defaultActivity === "bathe my dog") {
+        return `This Saturday, I want to ${defaultActivity}!`
+    } else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+console.log(saturdayFun())
+
+function mondayWork(work) {  
+    if (work === "work from home") {
+        return `This Monday, I will ${work}.!`
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+console.log(mondayWork())
+
+function wrapAdjective() {
+    
+}
