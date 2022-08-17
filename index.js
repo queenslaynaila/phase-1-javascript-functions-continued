1a8c0f89d75b5c82fb12dffd63394ec517e8a543
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-17 15:05:54 +0300

    Done with assignent

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 function saturdayFun(defaultActivity) {
-    if (defaultActivity === "bathe my dog") {
-        return `This Saturday, I want to ${defaultActivity}!`
-    } else {
-        return "This Saturday, I want to roller-skate!"
+  if (defaultActivity === "bathe my dog") {
+    return `This Saturday, I want to ${defaultActivity}!`;
+  } else {
+    return "This Saturday, I want to roller-skate!";
+  }
+}
+
+console.log(saturdayFun());
+
+function mondayWork(work) {
+  if (work === "work from home") {
+    return `This Monday, I will ${work}.`;
+  } else {
+    return "This Monday, I will go to the office.";
+  }
+}
+
+console.log(mondayWork());
+
+function wrapAdjective(symbol,emphatic) { 
+    if (symbol === "*") {
+        return `You are *${emphatic}*!`
+    } else if (symbol === "||") {
+        return `You are ${symbol}${emphatic}${symbol}!`
     }
 }
 
-console.log(saturdayFun())
+console.log(wrapAdjective("||" , "a dedicated programmer"))
 
-function mondayWork(work) {  
-    if (work === "work from home") {
-        return `This Monday, I will ${work}.!`
-    } else {
-        return "This Monday, I will go to the office."
-    }
-}
-
-console.log(mondayWork())
-
-function wrapAdjective() {
-    
-}
